6f876e8ec16fff5df7fca7a3bfb09da0c5850799
Igor Popov <popov.nirvana@gmail.com>
2024-12-12 07:06:17 +0300

    + Library build scripts

---
+++ build_libcxx.ps1
@@ -0,0 +1,4 @@
+﻿$llvm_root = "./llvm-project"
+$build_dir = "./build"
+cmake -G Ninja -S "$llvm_root/runtimes" -B $build_dir -DLLVM_ENABLE_PROJECTS="clang" `
+    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
