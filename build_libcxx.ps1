5488174a4236db49918b1c2d7404d4548b62cd0c
Igor Popov <popov.nirvana@gmail.com>
2024-12-16 08:18:33 +0300

    Add shallow llvm-project

--- build_libcxx.ps1
+++ build_libcxx.ps1
@@ -3,8 +3,10 @@
 $llvm_root = "./llvm-project"
 $build_dir = "./build/llvm"
 cmake -G Ninja -S "$llvm_root/llvm" -B $build_dir -DLLVM_ENABLE_PROJECTS="clang" `
-    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
+    -DCMAKE_BUILD_TYPE=Release
+#    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
 
-ninja -C $build_dir runtimes
-ninja -C $build_dir check-runtimes
+cmake --build $build_dir
+#ninja -C $build_dir runtimes
+#ninja -C $build_dir check-runtimes
 #ninja -C $build_dir install-runtimes
