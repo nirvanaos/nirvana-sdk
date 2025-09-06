c739a3f09e4e13ee893262173ca62380d3866363
Igor Popov <popov.nirvana@gmail.com>
2025-09-06 08:34:03 +0300

    Switch to CodeView debug format

--- toolchain.cmake
+++ toolchain.cmake
@@ -48,7 +48,8 @@ set (CMAKE_CXX_FLAGS_INIT ${cpp_compile_flags})
 set (CMAKE_C_FLAGS_INIT ${c_compile_flags})
 set (CMAKE_ASM_FLAGS_INIT ${c_compile_flags})
 
-set (debug_flags "-gdwarf-4")
+#set (debug_flags "-gdwarf-4")
+set (debug_flags "-gcodeview")
 set (release_flags "-fno-builtin")
 
 set (CMAKE_CXX_FLAGS_DEBUG_INIT ${debug_flags})
