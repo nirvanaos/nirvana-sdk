b4b699fabd0dfe671b169a6d7c967c765a6009c4
Igor Popov <popov.nirvana@gmail.com>
2025-09-10 13:13:25 +0300

    Pull the latest LLVM master

--- pull_llvm.ps1
+++ pull_llvm.ps1
@@ -1,5 +1,5 @@
 $llvm_url = "https://github.com/llvm/llvm-project.git"
-$llvm_tag = "llvmorg-21.1.0"
+$llvm_tag = "main" # "llvmorg-21.1.0"
 $llvm_root = "./llvm-project"
 if (!(Test-Path "$llvm_root\.git")) {
 	git clone --branch $llvm_tag --depth 1 $llvm_url $llvm_root
