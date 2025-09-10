4d4d4f25aa4b3e415ddb6e82cb7dd3f74a3be7a7
Igor Popov <popov.nirvana@gmail.com>
2025-09-10 13:19:32 +0300

    Rely on vcpkg find MSVC installation

--- install_packages.ps1
+++ install_packages.ps1
@@ -8,16 +8,12 @@ if ($args.count -ge 1) {
 	$platform = "x64"
 }
 
-& "$PSScriptRoot\vsdevshell.ps1"
-
 switch ($platform) {
 	"x64" {
 		$vcpkg_triplet = "x64-windows"
-		Enter-VsDevShell -VsInstallPath:"$visualStudioPath" -SkipAutomaticLocation -HostArch amd64 -Arch amd64
 	}
 	"x86" {
 		$vcpkg_triplet = "x86-windows"
-		Enter-VsDevShell -VsInstallPath:"$visualStudioPath" -SkipAutomaticLocation -HostArch amd64 -Arch x86
 	}
 	default {
 		Write-Host "Unknown platform"
